ad61bb3dd024f26358046de520cf997e04d2e152
Артур Чигрин <it5@stekomail.com.ua>
2024-05-31 10:54:41 +0300

    notifyjs

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -6,6 +6,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import { createApp } from 'vue';
 const app = createApp({});
 
+import {successNotify, errorNotify, infoNotify, warningNotify} from "./settings/notify.js";
+app.config.globalProperties.$successNotify = successNotify;
+app.config.globalProperties.$errorNotify = errorNotify;
+app.config.globalProperties.$infoNotify = infoNotify;
+app.config.globalProperties.$warningNotify = warningNotify;
+
 import Test from "./components/Test.vue";
 app.component("Test", Test);
 
